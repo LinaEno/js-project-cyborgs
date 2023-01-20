dd3dcc8554ffa1e0a22c3be81a6c84f6498b82b9
Lina Budkova <linabudkova35@gmail.com>
2023-01-20 12:03:38 +0200

    wqeqwe

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
-const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    openModalLink: document.querySelector('.js-card-item'),
-}
+export const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  openModalLink: document.querySelector('.js-card-item'),
+  formWarning: document.querySelector('.header-form__warning'),
+};
---
+++ src/js/searchfield.js
@@ -0,0 +1,33 @@
+import { FilmApi } from './FilmAPI';
+import { refs } from './refs';
+
+const filmApi = new FilmApi();
+console.log(filmApi);
+
+const onSearchFormSubmit = async event => {
+  event.preventDefault();
+
+  filmApi.searchQuery = event.target.elements.query.value.trim();
+  filmApi.page = 1;
+  refs.formWarning.textContent = '';
+
+  try {
+    const data = await filmApi.getFilmsByName();
+    console.log(data);
+
+    if (!data.results.length) {
+      setTimeout(() => {
+        refs.formWarning.classList.add('is-hidden');
+      }, 5000);
+      refs.formWarning.classList.remove('is-hidden');
+      refs.formWarning.textContent =
+        'Search result not successful. Enter the correct movie name and try again.';
+      event.target.reset();
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
