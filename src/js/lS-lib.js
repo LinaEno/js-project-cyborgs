7986c9908f1b4fb00fb04aae025c7e46a7d0f045
Lina Budkova <linabudkova35@gmail.com>
2023-01-24 17:55:38 +0200

    qeqwe

--- src/js/lS-lib.js
+++ src/js/lS-lib.js
@@ -1,32 +1,33 @@
-import { renderMarkupLib } from "./renderLib";
-import { refs } from "./refs-lib";
+import { renderMarkupLib } from './renderLib';
+import { refs } from './refs-lib';
+import img from '../../images/lenta.png';
 
 refs.watchedBtnLib.addEventListener('click', renderWatchedFilms);
 
 refs.queueBtnLib.addEventListener('click', renderQueueFilms);
 
 function renderWatchedFilms() {
-    const films = JSON.parse(localStorage.getItem("watched")) || [];
-    if (!films.length) {
-        refs.filmLibList.innerHTML = "Not films"
-        return;
-    }
-    renderMarkupLib(films);
-    refs.watchedBtnLib.classList.add('active');
-    refs.queueBtnLib.classList.remove('active');
-
-
+  const films = JSON.parse(localStorage.getItem('watched')) || [];
+  if (!films.length) {
+    refs.filmLibList.innerHTML = `<p class="lib-text">There are no films here yet</p>
+<img src="../images/lenta.png" alt="lenta" width="500">`;
+    return;
+  }
+  renderMarkupLib(films);
+  refs.watchedBtnLib.classList.add('active');
+  refs.queueBtnLib.classList.remove('active');
 }
 
 function renderQueueFilms() {
-    const films = JSON.parse(localStorage.getItem("queue")) || [];
-    if (!films.length) {
-        refs.filmLibList.innerHTML = "Not films"
-        return
-    }
-    renderMarkupLib(films);
-    refs.queueBtnLib.classList.add('active');
-    refs.watchedBtnLib.classList.remove('active');
+  const films = JSON.parse(localStorage.getItem('queue')) || [];
+  if (!films.length) {
+    refs.filmLibList.innerHTML = `<p class="lib-text">There are no films here yet</p>
+<img src="../images/lenta.png" alt="lenta">`;
+    return;
+  }
+  renderMarkupLib(films);
+  refs.queueBtnLib.classList.add('active');
+  refs.watchedBtnLib.classList.remove('active');
 }
 
 renderWatchedFilms();
