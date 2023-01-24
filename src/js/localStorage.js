f4b6161b4bf358d09a08b56d2747981d3bc274c7
Lina Budkova <linabudkova35@gmail.com>
2023-01-24 13:24:37 +0200

    qweqwe

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,58 +1,70 @@
-import { renderMarkup } from "./pagination";
-import { refs } from "./refs";
-import { FilmAPI } from "./FilmAPI";
+import { renderMarkup } from './pagination';
+import { refs } from './refs';
+import { FilmAPI } from './FilmAPI';
 
 const filmApi = new FilmAPI();
 
 export async function onWatchedBtnClick(e) {
-    e.preventDefault();
-    console.log(e);
+  e.preventDefault();
+
+  try {
     const id = e.target.dataset.modalWatchedId;
 
-console.log(id);
-    try {
-    const results = await filmApi.getFilmDetails(id);;
-    console.log(results);
-    const watchedFilms = JSON.parse(localStorage.getItem("watched")) || [];
-        if (watchedFilms.some(film => film.id === +id)) {
-            console.log('qwe');
-            return;
-        } 
-        watchedFilms.push(results);
-    
+    if (e.target.dataset.action === 'add') {
+      const results = await filmApi.getFilmDetails(id);
+      console.log(results);
+      const watchedFilms = JSON.parse(localStorage.getItem('watched')) || [];
+      if (watchedFilms.some(film => film.id === +id)) {
+        console.log('qwe');
+        return;
+      }
+      watchedFilms.push(results);
 
-    localStorage.setItem("watched", JSON.stringify(watchedFilms));
-    refs.watchedBtn.textContent = "Remove from watched";
-        
-    } catch (error) {
-    console.log(error);
+      localStorage.setItem('watched', JSON.stringify(watchedFilms));
+      e.target.textContent = 'Remove from watched';
+      e.target.dataset.action = 'remove';
+    } else {
+      const watchedFilms = JSON.parse(localStorage.getItem('watched')) || [];
+      console.log(watchedFilms);
+      const filteredwatchedFilms = watchedFilms.filter(film => film.id !== +id);
+      console.log(filteredwatchedFilms);
+      localStorage.setItem('watched', JSON.stringify(filteredwatchedFilms));
+      e.target.textContent = 'Add to watch';
+      e.target.dataset.action = 'add';
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function onQueueBtnClick(e) {
-    e.preventDefault();
-console.log(e);
+  e.preventDefault();
+
+  try {
     const id = e.target.dataset.modalQueueId;
-console.log(id);
-    try {
-    const results = await filmApi.getFilmDetails(id);
-    console.log(results);
-        const queuedFilms = JSON.parse(localStorage.getItem("queue")) || [];
-        if (queuedFilms.some(film => film.id === +id)) {
-            return;
-        } 
-        queuedFilms.push(results);
-        console.log(queuedFilms);
-        localStorage.setItem("queue", JSON.stringify(queuedFilms));
-        refs.queueBtn.textContent = "Remove from queue";
-        refs.queueBtn.classList.add('remove');
-        // if (refs.queueBtn.classList.contains('remove')) {
-        //     localStorage.removeItem(results)
-        // }
 
+    if (e.target.dataset.action === 'add') {
+      const results = await filmApi.getFilmDetails(id);
 
-        
-    } catch (err) {
-    console.log(err);
+      const queuedFilms = JSON.parse(localStorage.getItem('queue')) || [];
+      if (queuedFilms.some(film => film.id === +id)) {
+        return;
+      }
+      queuedFilms.push(results);
+
+      localStorage.setItem('queue', JSON.stringify(queuedFilms));
+      e.target.textContent = 'Remove from queue';
+      e.target.dataset.action = 'remove';
+    } else {
+      const queuedFilms = JSON.parse(localStorage.getItem('queue')) || [];
+      console.log(queuedFilms);
+      const filteredQueueFilms = queuedFilms.filter(film => film.id !== +id);
+      console.log(filteredQueueFilms);
+      localStorage.setItem('queue', JSON.stringify(filteredQueueFilms));
+      e.target.textContent = 'Add to queue';
+      e.target.dataset.action = 'add';
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
