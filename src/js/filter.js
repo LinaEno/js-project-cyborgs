26fd39ac0cfb834f8e5626ce952209376c921554
Lina Budkova <linabudkova35@gmail.com>
2023-01-25 15:54:50 +0200

    йцуйцуйу

--- src/js/filter.js
+++ src/js/filter.js
@@ -10,29 +10,6 @@ filterContainerVote.addEventListener('change', onVotesFilter);
 const filterClear = document.querySelector('.js-clear');
 filterClear.addEventListener('click', onClickClear);
 
-// function onFilterChange(e) {
-//   e.preventDefault();
-
-//   const currentId = +e.currentTarget.value;
-//   console.log(currentId);
-//   fetch(
-//     'https://api.themoviedb.org/3/trending/movie/day?api_key=2963fc82afd3cb57f64d050a1ba5935c&language=en-US'
-//   )
-//     .then(resp => resp.json())
-//     .then(data => {
-//       const id = currentId; //беремо значення з тегу селект
-//       console.log(id);
-//       console.log(data);
-//       console.log(data.results);
-
-//       const byGanre = data.results.filter(film =>
-//         film['genre_ids'].includes(id)
-//       );
-
-//       console.log(byGanre);
-//       refs.cardsListEl.innerHTML = renderMarkup(byGanre);
-//     });
-// }
 const filter = {
   genre_id: '',
   vote: '',
@@ -84,10 +61,9 @@ async function onVotesFilter(e) {
   e.preventDefault();
   let vote = +e.currentTarget.value;
   filter.vote = vote;
-  console.log(vote);
+
   const results = await getFilteredMovies();
-  console.log(results);
-  console.log(results.results);
+
   refs.cardsListEl.innerHTML = renderMarkup(results.results);
 }
 
